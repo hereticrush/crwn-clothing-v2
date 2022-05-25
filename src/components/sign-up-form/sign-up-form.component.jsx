fe4d9b1c1cc0cf5812b5b82ff69e1fb24c7c0baa
hereticrush <auroraborealis853@gmail.com>
2022-05-26 00:02:14 +0300

    Add button component for different button types

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import { async } from '@firebase/util';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 const defaultFormFields = {
     displayName: '',
@@ -49,8 +50,9 @@ const SignUpForm = () => {
     }
 
     return(
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput
                     label='Display Name'
@@ -96,7 +98,7 @@ const SignUpForm = () => {
                     }}
                 />
 
-                <button type="submit">Sign Me Up</button>
+                <Button type='inverted'>Sign Me Up</Button>
             </form>
         </div>
     );
